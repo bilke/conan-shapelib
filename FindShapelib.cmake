95ae5b6f0805ebc7f7f9154d4630f582dbb91e0c
Lars Bilke <lars.bilke@ufz.de>
2016-04-22 10:28:30 +0200

    Simplified FindShapelib.cmake

--- FindShapelib.cmake
+++ FindShapelib.cmake
@@ -6,16 +6,13 @@ find_library(Shapelib_LIBRARY NAMES ${CONAN_LIBS_SHAPELIB}
 	PATHS ${CONAN_LIB_DIRS_SHAPELIB}
 	NO_DEFAULT_PATH)
 
-set(Shapelib_FOUND TRUE)
-
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Shapelib DEFAULT_MSG
-	Shapelib_FOUND
-	Shapelib_INCLUDE_DIR
+find_package_handle_standard_args(Shapelib REQUIRED_VARS
 	Shapelib_LIBRARY
+	Shapelib_INCLUDE_DIR
 )
 
 set(Shapelib_INCLUDE_DIRS ${Shapelib_INCLUDE_DIR})
 set(Shapelib_LIBRARIES ${Shapelib_LIBRARY})
 
-mark_as_advanced(Shapelib_INCLUDE_DIR Shapelib_LIBRARY Shapelib_DIR)
+mark_as_advanced(Shapelib_INCLUDE_DIR Shapelib_LIBRARY)
